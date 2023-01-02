32a38c06e1706a8255cd3269219f30848b9f7596
Keks <keks@htmlacademy.ru>
2023-01-02 14:32:02 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/script.js
@@ -0,0 +1,15 @@
+const toggleButtonElement = document.querySelector('.main-nav__toggle');
+const mainNavElement = document.querySelector('.main-nav');
+const mainHeaderElement = document.querySelector('.main-header');
+
+mainHeaderElement.classList.remove('main-nav--nojs');
+
+toggleButtonElement.addEventListener('click', () => {
+  if (mainNavElement.classList.contains('main-nav--closed')) {
+    mainNavElement.classList.remove('main-nav--closed');
+    mainNavElement.classList.add('main-nav--opened');
+  } else {
+    mainNavElement.classList.add('main-nav--closed');
+    mainNavElement.classList.remove('main-nav--opened');
+  }
+});
