0f5ad09a074828ee2587527987bd098f1a0062a4
anufrievama <anufrievamaria89@gmail.com>
2023-01-16 01:08:56 +0300

    Редактирует стилизацию и верстку страниц

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,7 +2,7 @@ const toggleButtonElement = document.querySelector('.main-nav__toggle');
 const mainNavElement = document.querySelector('.main-nav');
 const mainHeaderElement = document.querySelector('.main-header');
 
-mainHeaderElement.classList.remove('main-nav--nojs');
+mainHeaderElement.classList.remove('main-header--nojs');
 
 toggleButtonElement.addEventListener('click', () => {
   if (mainNavElement.classList.contains('main-nav--closed')) {
